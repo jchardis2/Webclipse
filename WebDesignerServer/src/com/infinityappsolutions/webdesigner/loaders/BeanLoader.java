39361e80dfe93d27bffebd2018c96c0d0863150e
Jimmy Hardison <jchardis2@gmail.com>
2014-02-11 16:48:16 -0500

    c

--- WebDesignerServer/src/com/infinityappsolutions/webdesigner/loaders/BeanLoader.java
+++ WebDesignerServer/src/com/infinityappsolutions/webdesigner/loaders/BeanLoader.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.infinityappsolutions.webdesigner.beans.UserBean;
+
 /**
  * This interface helps enforce the paradigm of what should be contained in a
  * loader.
